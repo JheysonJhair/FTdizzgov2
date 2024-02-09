2bf59e767583167b45f66ca87443aa71de8f139a
JheysonJhair <jhair2003unamba@gmail.com>
2024-02-09 00:06:16 -0500

    fix: correcion bug

--- src/screens/perfil/EditPerfil.js
+++ src/screens/perfil/EditPerfil.js
@@ -1,191 +1,251 @@
 import React, { useState, useEffect } from "react";
 import {
-  GiftedChat,
-  Bubble,
-  Send,
-  Composer,
-  InputToolbar,
-} from "react-native-gifted-chat";
-import { TouchableOpacity, View, Text, Image } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
-import StatusModal from "../../components/modals/StatusModal ";
-import { getMensajes } from "../../api/apiChat";
-import { sendMessage } from "../../api/apiChat";
-import { useUser } from "../../components/utils/UserContext";
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import {
+  useFonts,
+  Montserrat_800ExtraBold,
+} from "@expo-google-fonts/montserrat";
 
-const Chat = () => {
-  const [messages, setMessages] = useState([]);
-  const [inputText, setInputText] = useState("");
-  const { userData } = useUser();
+import TextArea from "../../components/forms/TextArea";
+import InputTwo from "../../components/forms/InputTwo";
+import Button from "../../components/forms/Button";
+
+import { useUser } from "../../components/utils/UserContext";
+import StatusModal from "../../components/modals/StatusModal ";
+import { updateProfile } from "../../api/apiPerfil";
+import { updateProfileImage } from "../../api/apiPerfil";
+const EditPerfil = () => {
+  const { userData, setUserInfo } = useUser();
+
+  const [initialUsuario, setInitialUsuario] = useState(
+    userData.UserName ||
+      userData.FirstName.replace(/\s+/g, "").toLowerCase() +
+        userData.LastName.split(" ")[0].replace(/\s+/g, "").toLowerCase()
+  );
+  const [initialDescripcion, setInitialDescripcion] = useState(
+    userData.Description ||
+      "Experimenté soledad en un vaso, sin encontrar la compañía que esperada."
+  );
+
   const [modalVisible, setModalVisible] = useState(false);
   const [modalStatus, setModalStatus] = useState("error");
   const [text, setText] = useState("");
   const [text2, setText2] = useState("");
 
-  useEffect(() => {
-    const fetchMensajes = async () => {
-      try {
-        const mensajes = await getMensajes();
-        setMessages(mensajes.reverse());
-      } catch (error) {
-        console.error("Error al obtener los mensajes:", error.message);
-      }
-    };
-    fetchMensajes();
+  const [usuario, setUsuario] = useState(initialUsuario);
+  const [descripcion, setDescripcion] = useState(initialDescripcion);
+  const [image, setImage] = useState(null);
+  const MAX_CARACTERES = 71;
 
-    const intervalId = setInterval(fetchMensajes, 500);
-    return () => clearInterval(intervalId);
+  const [fontsLoaded] = useFonts({
+    Montserrat_800ExtraBold,
+  });
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Permiso denegado para acceder a la galería de imágenes.");
+        }
+      }
+    })();
   }, []);
 
-  const onSend = async (newMessages = [], image = null) => {
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      const formData = new FormData();
+      formData.append("file", {
+        uri: result.uri,
+        type: "image/jpeg",
+        name: "file",
+      });
+
+      const updateResult = await updateProfileImage(userData.IdUser, formData);
+
+      if (updateResult.success) {
+        setImage(result.uri);
+        setModalStatus("success");
+        setModalVisible(true);
+        setText("Actualizado con exito");
+        setText2("Su perfil se subió exitosamente!.");
+        setTimeout(() => {
+          setModalVisible(false);
+        }, 2000);
+        setUserInfo({ ...userData, ProfileImage: result.uri });
+      } else {
+        console.error(
+          "Error al actualizar la imagen de perfil:",
+          updateResult.error
+        );
+      }
+    }
+  };
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const handleUsuarioChange = (text) => {
+    setUsuario(text);
+  };
+
+  const handleDescripcionChange = (text) => {
+    if (text.length <= MAX_CARACTERES) {
+      setDescripcion(text);
+    }
+  };
+
+  const onHandleUpdate = async () => {
     try {
-      if (newMessages === "" && !image) {
+      const formData = new FormData();
+      formData.append("UserName", usuario);
+      formData.append("Description", descripcion);
+      formData.append("IdUser", userData.IdUser);
+
+      const { success, error } = await updateProfile(formData);
+
+      if (success) {
+        setUserInfo({
+          ...userData,
+          UserName: usuario,
+          Description: descripcion,
+        });
+        setModalStatus("loading");
+        setModalVisible(true);
+        setText("Verificando...");
+        setText2(
+          "Recuerda que no podrás cambiar el nombre de usuario pasado 7 días."
+        );
+      } else {
         setModalStatus("error");
         setModalVisible(true);
-        setText("Mensaje vacío");
-        setText2("Tu mensaje no contiene nada!");
-        return;
-      } else {
-        const userId = userData.IdUser;
-        await sendMessage(userId, newMessages, image);
-
-        setInputText("");
+        setText("Opps! algo salió mal");
+        setText2("Intentalo mas tarde porfavor.");
       }
     } catch (error) {
-      console.error("Error al enviar mensaje:", error.message);
-    }
-  };
-
-  const renderSendButton = (props) => {
-    const { text } = props;
-    
-    if (text && text.trim().length > 0) {
-      return (
-        <TouchableOpacity onPress={() => onSend(props.text)}>
-          <Icon
-            name="arrow-circle-right"
-            size={36}
-            color="#66bef4"
-            style={{ marginLeft: 10, marginRight: 10, color: "#66bef4" }}
-          />
-        </TouchableOpacity>
-      );
-    }
-  
-    return null;
-  };
-
-  const handleChoosePhoto = async () => {
-    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert("Permiso para acceder a la galería de imágenes necesario");
-      return;
-    }
-
-    const pickerResult = await ImagePicker.launchImageLibraryAsync();
-
-    if (!pickerResult.cancelled) {
-      onSend([], pickerResult.uri);
-    }
-  };
-
-  const CustomInputToolbar = (props) => {
-    return (
-      <InputToolbar
-        {...props}
-        containerStyle={{ backgroundColor: "#161B21" }}
-      />
-    );
-  };
-
-  useEffect(() => {
-    if (modalVisible) {
-      const timeout = setTimeout(() => {
+      console.error("Error:", error);
+    } finally {
+      setTimeout(() => {
         setModalVisible(false);
-      }, 2000);
-
-      return () => clearTimeout(timeout);
+      }, 3000);
     }
-  }, [modalVisible]);
+  };
 
   return (
-    <>
-      <GiftedChat
-        messages={messages}
-        showAvatarForEveryMessage={false}
-        showUserAvatar={false}
-        onSend={(newMessages) => onSend(newMessages)}
-        messagesContainerStyle={{
-          backgroundColor: "#161B21",
-        }}
-        textInputStyle={{
-          backgroundColor: "#212834",
-          borderRadius: 10,
-          paddingLeft: 10,
-          marginTop: 2,
-          color: "#fff",
-        }}
-        placeholder="Mensaje"
-        user={{
-          _id: userData.IdUser,
-          name: userData.FirstName,
-        }}
-        renderBubble={(props) => (
-          <View>
-            {props.currentMessage.user._id !== userData.IdUser && (
-              <Text
-                style={{ color: "#40A5E7", fontWeight: "bold", fontSize: 11 }}
-              >
-                {props.currentMessage.user.name}
-              </Text>
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.profileContainer}>
+          <TouchableOpacity style={styles.editButton} onPress={pickImage}>
+            {image ? (
+              <Image source={{ uri: image }} style={styles.profileImage} />
+            ) : (
+              <Image
+                source={{ uri: userData.ProfileImage }}
+                style={styles.profileImage}
+              />
             )}
-            <Bubble
-              {...props}
-              wrapperStyle={{
-                left: {
-                  backgroundColor: "#212834",
-                  borderRadius: 10,
-                },
-                right: {
-                  backgroundColor: "#40A5E7",
-                  borderRadius: 10,
-                },
-              }}
-              textStyle={{
-                left: {
-                  color: "#fff",
-                },
-                right: {
-                  color: "#fff",
-                },
-              }}
+          </TouchableOpacity>
+          <Text style={styles.labelFoto}>Cambiar foto</Text>
+        </View>
+        <View style={styles.containerForm}>
+          <Text style={styles.labelTu}>Acerca de ti</Text>
+          <View style={styles.formSection}>
+            <Text style={styles.label}>Nombre de usuario</Text>
+            <InputTwo
+              placeholder="Usuario"
+              onChangeText={handleUsuarioChange}
+              value={usuario}
             />
           </View>
-        )}
-        renderSend={renderSendButton}
-        renderComposer={(props) => (
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <Composer {...props} text={inputText} />
-            {renderSendButton(props)}
+          <View style={styles.formSection}>
+            <Text style={styles.label}>Descripción Corta</Text>
+            <TextArea
+              placeholder="Descripción"
+              onChangeText={handleDescripcionChange}
+              value={descripcion}
+            />
+            <Text style={styles.caracteresRestantes}>
+              {MAX_CARACTERES - descripcion.length} caracteres restantes
+            </Text>
           </View>
-        )}
-        renderInputToolbar={CustomInputToolbar}
-        onInputTextChanged={(text) => setInputText(text)}
-        text={inputText}
-      />
-      <TouchableOpacity onPress={handleChoosePhoto}>
-        <Text>Seleccionar imagen</Text>
-      </TouchableOpacity>
+        </View>
+        <View style={styles.containerButon}>
+          <Button title="Actualizar Perfil" onPress={onHandleUpdate} />
+        </View>
+      </View>
       <StatusModal
         visible={modalVisible}
         status={modalStatus}
         text={text}
         text2={text2}
       />
-    </>
+    </KeyboardAvoidingView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#161B21",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  profileContainer: {
+    alignItems: "center",
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
+  containerForm: {
+    width: "80%",
+  },
+  containerButon: {
+    width: "85%",
+    marginBottom: 90,
+  },
+  formSection: {
+    marginBottom: 10,
+  },
+  caracteresRestantes: {
+    fontSize: 12,
+    color: "#8c8c8c",
+  },
+  label: {
+    color: "#fff",
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  labelFoto: {
+    color: "#a6a6a6",
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  labelTu: {
+    color: "#40A5E7",
+    fontSize: 16,
+    marginBottom: 8,
+  },
+});
 
-export default Chat;
+export default EditPerfil;
